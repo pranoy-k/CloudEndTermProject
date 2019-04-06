941127bf5760f036bc1cec6ebfc7bdbf0fc8c789
kpranoy <kpranoy@tamu.edu>
2019-04-06 11:42:18 -0500

    First Working Repositary

---
+++ Raft/messages/response.py
@@ -0,0 +1,9 @@
+from Raft.messages.base import BaseMessage
+
+
+class ResponseMessage(BaseMessage):
+
+    _type = BaseMessage.Response
+
+    def __init__(self, sender, receiver, term, data):
+        BaseMessage.__init__(self, sender, receiver, term, data)
---
+++ test_MemoryBoard.py
@@ -0,0 +1,29 @@
+import unittest
+
+from Raft.boards.memory_board import MemoryBoard
+from Raft.messages.base import BaseMessage
+
+class TestMemoryBoard( unittest.TestCase ):
+
+	def setUp( self ):
+		self.board = MemoryBoard()
+
+	def test_memoryboard_post_message( self ):
+
+		msg = BaseMessage( 0, 0, 0, 0 )
+		self.board.post_message( msg )
+		self.assertEquals( msg, self.board.get_message() )
+
+	def test_memoryboard_post_message_make_sure_they_are_ordered( self ):
+
+		msg = BaseMessage( 0, 0, 0, 0 )
+		msg2 = BaseMessage( 0, 0, 0, 0 )
+		msg2._timestamp -= 100
+
+		self.board.post_message( msg )
+		self.board.post_message( msg2 )
+
+		self.assertEquals( msg2, self.board.get_message() )
+
+if __name__ == '__main__':
+    unittest.main()
