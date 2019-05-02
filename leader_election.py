79198550578bfaa18389858fdd49a3a2e4d3b790
kpranoy <kpranoy@tamu.edu>
2019-05-02 16:06:17 -0500

    Printing cleared

--- leader_election.py
+++ leader_election.py
@@ -1,5 +1,11 @@
+from __future__ import print_function
+
+## Importing all the required libraries
 import unittest
 import time
+import threading
+
+from threading import Thread
 
 # from Raft.boards.memory_board import MemoryBoard
 from Raft.messages.append_entries import AppendEntriesMessage
@@ -9,13 +15,16 @@ from Raft.states.follower import Follower
 from Raft.states.candidate import Candidate
 from Raft.states.leader import Leader
 
-
-from threading import Thread
+lock = threading.Lock()
 
 def checkMesages():
+    i = 0
     while(True):
-        # print("x")
         time.sleep(0.0001)
+        if i % 10000 == 0:
+            lock.acquire()
+            print('.')
+            lock.release()
         for name in range(len(followers)):
             while(True):
                 message = followers[name].get_message()
@@ -23,17 +32,15 @@ def checkMesages():
                 if message == None:
                     break
                 else:
-                    # print("Got message")
                     followers[name]._state.on_message(message)
+        i += 1
 
-
+## Function associated with each server
 def serverFunction(name):
-    # print(name)
     global followers
     server = followers[name]
     timeoutTime = time.time()+1
     # if isinstance(server._state) == Follower:
-    print("Started server with name ", name)
     # elif server._serverState == resumeState:
     #     print("Resumed server with name ", name)
     #     print(server._commitIndex)
@@ -45,28 +52,20 @@ def serverFunction(name):
 
     # count =0
     while(True):
-        num = 0
-        
-        # print(name)
+
+        ## Leader Sending Heartbeat
         if type(server._state) == Leader:
             # print("Server is now leader: ", server._name)
             if time.time()-timeoutTime >0.5:
                 server._state._send_heart_beat()
                 timeoutTime = time.time()+0.5
-
         # if type(server._state) == Candidate and time.time() >= server._state._timeoutTime:
         #     server._state = Follower()
         #     server._state.set_server(server)
         #     count += 1
 
         if type(server._state) == Follower:
-            # print("time.time: ", time.time()/1000000)
-            # print("server._state._timeoutTime: ",
-            #       server._state._timeoutTime/1000000)
             if time.time() >= server._state._timeoutTime:
-                # print("server._state._timeoutTime: ", server._state._timeoutTime/1000000)
-                print(time.time(),server._state._timeoutTime)
-                print("server ", server._name, "finds that the leader is dead")
                 server._state = Candidate()
                 server._state.set_server(server)
             # count += 1
@@ -89,21 +88,25 @@ def serverFunction(name):
         #         server._state = Candidate()
         #         server._state.set_server(server)
 
+print("\n\nCreating four servers with names ranging from 0-3")
+# print("\nThe current term is 0")
 
 # Create Servers
 followers = []
-for i in range(0, 5):
-    # board = MemoryBoard()
+for name in range(4):
     state = Follower()
-    server = Server(i, state, [], [])
+    server = Server(name, state, [], [])
 
     for follower in followers:
         follower._neighbors.append(server)
         server._neighbors.append(follower)
 
     followers.append(server)
-    thread = Thread(target=serverFunction, args=(i,))
+    thread = Thread(target=serverFunction, args=(name,))
     thread.start()
+    print ("Started server with name ", name)
+print("\nWait until first timer timesout")
+
 
 thread = Thread(target=checkMesages, args=())
 thread.start()
